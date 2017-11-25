08a502617a00448f28a4f02dff6ddfa31c1839a1
Tommy Schouw <grubsnik@gmail.com>
2017-11-25 15:49:59 +0100

    initial commit

---
+++ StellarisConsistencyChecker/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StellarisConsistencyChecker
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Tech> techs = new List<Tech>();
+			int techsFound = 0, techsWithErrors = 0, errorsFound = 0;
+			string path = @"c:\Program Files (x86)\Steam\steamapps\common\Stellaris\common\technology\";
+			foreach (string filepath in Directory.EnumerateFiles(path))
+			{
+				string tmp = File.ReadAllText(filepath);
+				var rawTechs = Regex.Split(tmp, "^tech_", RegexOptions.Multiline);
+				for (int i = 1; i < rawTechs.Count(); i++)
+				{
+					Tech t = new Tech(rawTechs[i]);
+					techsFound++;
+					if (t.errors.Count > 0)
+					{
+						techsWithErrors++;
+						Console.WriteLine(string.Format("Technology tech_{0}({1}) has the following inconsistensies:", t.name, Regex.Match(filepath, "[a-zA-Z0-9_]+\\.txt$").Value));
+						foreach (string error in t.errors)
+						{
+							errorsFound++;
+							Console.WriteLine(error);
+						}
+						Console.WriteLine(string.Empty);
+					}
+					techs.Add(t);
+				}
+			}
+
+			Console.WriteLine(string.Format("Summary, {0} techs scanned of which {1} has 1 or more inconsistencies, for a total issue count of {2}", techsFound, techsWithErrors, errorsFound));
+			Console.ReadKey();
+		}
+	}
+}
---
+++ StellarisConsistencyChecker/tech.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StellarisConsistencyChecker
+{
+	public class Tech
+	{
+		private string raw;
+		private string category;
+		private string area;
+		public string name;
+		public List<string> errors;
+		private string tier;
+		private string costTier;
+		private string costSubTier;
+		private string costString;
+
+
+		public Tech(string raw)
+		{
+			this.raw = raw;
+			this.errors = new List<string>();
+			this.name = Regex.Match(raw, "^([a-zA-Z0-9_]+)").Groups[0].Value;
+			this.category = Regex.Match(raw, "category = { ([a-zA-Z0-9_]+) }").Groups[1].Value;
+			this.area = Regex.Match(raw, "area = ([a-zA-Z0-9_]+)").Groups[1].Value;
+			this.tier = Regex.Match(raw, "tier = ([a-zA-Z0-9_@]+)").Groups[1].Value;
+			var cost = Regex.Match(raw, "cost = (@tier([0-9]+)cost([0-9]+))");
+			this.costTier = cost.Groups[2].Value;
+			this.costSubTier = cost.Groups[3].Value;
+			this.costString = cost.Groups[1].Value; 
+			this.PerformValidation();
+		}
+
+		private void PerformValidation()
+		{
+			this.CheckAreaAndCategory();
+			this.CheckTierAndCost();
+			this.CheckAreaConsistency();
+			this.CheckLeaderTraitConsistency();
+		}
+
+		private void CheckLeaderTraitConsistency()
+		{
+			if (this.FilterIrregularTechs() && !this.raw.Contains(string.Format("has_trait = \"leader_trait_expertise_{0}\"", this.category)))
+			{
+				this.errors.Add(string.Format("\tTechnology belongs to category {0}, but this expertise is not referenced in the tech", this.category));
+			}
+		}
+
+		private void CheckAreaConsistency()
+		{
+			var matches = Regex.Matches(this.raw, "area = ([a-zA-Z0-9_]+)");
+			foreach (Match match in matches)
+			{
+				if (match.Groups[1].Value.CompareTo(this.area) != 0)
+				{
+					this.errors.Add(string.Format("\tContains at least 2 different area references ({0}) and ({1})", this.area, match.Groups[1]));
+					break;
+				}
+			}
+		}
+
+		private void CheckAreaAndCategory()
+		{
+			switch (this.area)
+			{
+				case "engineering":
+					if (!(category == "materials" || category == "voidcraft" || category == "rocketry" || category == "industry"))
+					{
+						this.errors.Add(string.Format("\tCategory {0} does not belong in area {1}", this.category, this.area));
+					}
+					break;
+				case "society":
+					if (!(category == "military_theory" || category == "biology" || category == "statecraft" || category == "psionics" || category == "new_worlds"))
+					{
+						this.errors.Add(string.Format("\tCategory {0} does not belong in area {1}", this.category, this.area));
+					}
+					break;
+				case "physics":
+					if (!(category == "voidcraft" || category == "particles" || category == "computing" || category == "industry" || category == "field_manipulation" || category == "psionics"))
+					{
+						this.errors.Add(string.Format("\tCategory {0} does not belong in area {1}", this.category, this.area));
+					}
+					break;
+				default:
+					this.errors.Add("Unknown area");
+					break;
+			}
+		}
+
+		private void CheckTierAndCost()
+		{
+			if (this.FilterIrregularTechs()
+				&& this.tier.CompareTo(this.costTier) != 0)
+			{
+				this.errors.Add(string.Format("\tTier ({0}) and cost ({1}) do not align", this.tier, this.costString));
+			}
+
+			if (string.IsNullOrEmpty(this.tier))
+			{
+				this.errors.Add("\tUnable to parse Tier Information, maybe it is missing?");
+			}
+		}
+
+		private bool FilterIrregularTechs()
+		{
+			return !string.IsNullOrEmpty(this.tier)
+							&& this.tier.CompareTo("@repeatableTechTier") != 0
+							&& int.Parse(this.tier) > 0
+							&& !this.raw.Contains("weight = 0");
+		}
+	}
+}
